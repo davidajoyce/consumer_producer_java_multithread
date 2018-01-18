6a0000b2348c8348f64f7d44b07c9bb0d02d07c4
davidajoyce <davidajoyce141@gmail.com>
2018-01-18 13:10:07 +0000

    the produce and consume functions with a shared resource, a linked list with comments

--- src/ConsumerProducer.java
+++ src/ConsumerProducer.java
@@ -14,6 +14,36 @@ import java.util.LinkedList;
         //cons_prod_multhread class
         public void produce() throws InterruptedException
         {
+        	int value = 0;
+            while (true)
+            {
+            	//no more than one thread can access the code 
+            	//inside this block (synchonized)
+                synchronized (this)
+                {
+                    // the producer thread waits while list
+                    // is full
+                    while (list.size()==capacity)
+                   
+                        wait();
+                        
+ 
+                    System.out.println("The Producer produced-"
+                                                  + value);
+ 
+                    // insert the jobs in the list
+                    
+                    list.add(value++);
+ 
+                    // notifies the consumer thread that
+                    // now it can start consuming
+                    notify();
+ 
+                    // so you can see what the program is doing
+                    // and not printed everything at once
+                    Thread.sleep(1000);
+                }
+            }
             
             
         }
@@ -21,6 +51,30 @@ import java.util.LinkedList;
         // Function called by the consumer thread
         public void consume() throws InterruptedException
         {
+        	while (true)
+            {
+                synchronized (this)
+                {
+                    // consumer thread waits while list
+                    // is empty
+                    while (list.size()==0)
+                    	
+                        wait();
+                        
+ 
+                    //to consume the first job in the list
+                    int val = list.removeFirst();
+ 
+                    System.out.println("The Consumer consumed-"
+                                                    + val);
+ 
+                    // Wake up the producer thread
+                    notify();
+ 
+                    // and sleep like in the produce() function
+                    Thread.sleep(1000);
+                }
+            }
             
         }
     }
